969427194133123807f03b65423b64e21f68c346
lauraklemba <laurakcordeiro2@gmail.com>
2025-08-13 11:15:05 -0300

    Add files via upload

---
+++ principal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "fracao.h"
+
+int main() {
+    fracao_t *f1, *f2;
+    f1 = criar(1, 2);
+    f2 = criar(3, 4);
+    fracao_t *f3 = somar(f1, f2);
+    fracao_t *f4 = subtrair(f1, f2);
+    fracao_t *f5 = multiplicar(f1, f2);
+    fracao_t *f6 = dividir(f1, f2);
+    imprimir(f1);
+    imprimir(f2);
+    imprimir(f3);
+    imprimir(f4);
+    imprimir(f5);
+    imprimir(f6);
+    destruir(f1);
+    destruir(f2);
+    destruir(f3);
+    destruir(f4);
+    destruir(f5);
+    destruir(f6);
+    
+    return 0;
+}
